ab92a71451fd5479a178c1d2228c92bac0b862b6
Darren <2681160+darrenpyw@users.noreply.github.com>
2026-04-20 16:51:26 +0800

    Set solidity compiler to 0.8.25 and above for slither solidity scanning

--- samples/TokenBridge.sol
+++ samples/TokenBridge.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Damn Vulnerable DeFi v4 (https://damnvulnerabledefi.xyz)
-pragma solidity =0.8.25;
+pragma solidity ^0.8.25;
 
 import {DamnValuableToken} from "../DamnValuableToken.sol";
 import {L1Forwarder} from "../withdrawal/L1Forwarder.sol";
