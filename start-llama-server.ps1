f7f743a81275d96a2db5bd4d3a5122b32b8a6d04
Darren <2681160+darrenpyw@users.noreply.github.com>
2026-06-12 21:25:24 +0800

    Cast user input to int

--- start-llama-server.ps1
+++ start-llama-server.ps1
@@ -7,9 +7,9 @@ $llmModels = @(
         Path = "./models.ini";
     },
     @{ Name = "unsloth Qwen3.5-4B-MTP-GGUF";
-        Path = "unsloth/Qwen3.5-4B-MTP-GGUF:Q8_0";
+        Path = "unsloth/Qwen3.5-4B-MTP-GGUF:UD-Q4_K_XL";
         Port = 8000;
-        Params = @("--jinja", "--no-mmproj", "--temperature", 0.3, "-cmoe")
+        Params = @("--jinja", "--no-mmproj", "--temperature", 0.3, "-cmoe", "-np", 1, "-fa", "on")
     },
     @{ Name = "Unsloth gemma-4-E4B-it-GGUF Q4_K_M";
         Path = "unsloth/gemma-4-E4B-it-GGUF:Q4_K_M";
@@ -30,8 +30,27 @@ $llmModels = @(
         Path = "unsloth/gemma-4-E4B-it-GGUF:UD-Q4_K_XL";
         Port = 8000;
         Params = @("-np", 4, "--jinja", "--no-mmproj", "-ctk", "q4_0", "-ctv", "q4_0", "-cmoe", "--mlock")
+    },
+    @{ Name = "nvidia/NVIDIA-Nemotron-3-Nano-4B-GGUF";
+        Path = "nvidia/NVIDIA-Nemotron-3-Nano-4B-GGUF:Q4_K_M";
+        Port = 8000;
+        Params = @("-np", 4, "--jinja", "--no-mmproj", "-ctk", "q4_0", "-ctv", "q4_0", "--mlock", "-ngl", "-1", "-dev", "Vulkan1", "-fa", "on", "-kvo", "--mmap")
+    },
+     @{ Name = "unsloth gemma-4-26B-A4B-it-GGUF UD-Q4_K_M";
+        Path = "unsloth/gemma-4-26B-A4B-it-GGUF:UD-Q4_K_M";
+        Port = 8000;
+        Params = @("-np", 1, "--jinja", "--no-mmproj", "--mlock", "--temp", "1.0", "--top-p", "0.95", "--top-k", "64", "-ngl", "8", "-c", "65536", "-fa", "on", "--mmap", "--no-kv-offload", "--no-cache-idle-slots")
+    },
+     @{ Name = "unsloth gemma-4-12b-it-GGUF UD-Q4_K_XL";
+        Path = "unsloth/gemma-4-12b-it-GGUF:UD-Q4_K_XL";
+        Port = 8000;
+        Params = @("-np", 1, "--jinja", "--no-mmproj", "--mlock", "--temp", "1.0", "--top-p", "0.95", "--top-k", "64", "-ngl", "auto", "-c", "65536", "-fa", "on", "--no-cache-idle-slots")
+    },
+     @{ Name = "unsloth gemma-4-12b-it-GGUF Q3_K_S";
+        Path = "unsloth/gemma-4-12b-it-GGUF:Q3_K_S";
+        Port = 8000;
+        Params = @("-np", 1, "--jinja", "--no-mmproj", "--mlock", "--temp", "1.0", "--top-p", "0.95", "--top-k", "64", "-ngl", "auto", "-c", "65536", "-fa", "on", "--no-cache-idle-slots")
     }
-    
 )
 
 # Display menu
@@ -43,7 +62,9 @@ for ($i = 0; $i -lt $llmModels.Count; $i++) {
 Write-Host "$($llmModels.Count + 1). Exit" -ForegroundColor Yellow
 
 # Get user selection
-$selection = Read-Host "`nSelect a model (1-$($llmModels.Count + 1))"
+[int]$selection = Read-Host "`nSelect a model (1-$($llmModels.Count + 1))"
+Write-Host "`n User entered $($selection)"
+Write-Host "`n"
 
 if ($selection -eq ($llmModels.Count + 1)) {
     Write-Host "Exiting..." -ForegroundColor Green
@@ -52,15 +73,14 @@ if ($selection -eq ($llmModels.Count + 1)) {
 
 # Get WSL ethernet IP address
 # $wslIp =  Get-NetIPAddress |  Where-Object { $_.InterfaceAlias -eq "vEthernet (WSL (Hyper-V firewall))" -and $_.AddressFamily -eq "IPv4" } | Select-Object -ExpandProperty IPAddress
-if ($selection -eq 1 -and $selection -le $llmModels.Count) {
-    $selectedIndex = $selection - 1
+if ($selection -eq 1) {
     $selectedModel = $llmModels[0]
     Write-Host "`nStarting llama-server with $($selectedModel.Path)" -ForegroundColor Green
     Write-Host "`n"
     # Start llama-server
     & llama-server --host 0.0.0.0 --port 8000 --models-preset $($selectedModel.Path)
 
-} elseif ($selection -ge 1 -and $selection -le $llmModels.Count) {
+} elseif ($selection -ge 2 ) {
     $selectedIndex = $selection - 1
     $selectedModel = $llmModels[$selectedIndex]
     
